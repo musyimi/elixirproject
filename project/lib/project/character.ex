e85315cfa547a1a07d0f0db7f54b2e3823517077
ngusu <musyimi@users.noreply.github.com>
2022-12-17 15:04:07 +0300

    designed the person entity using structs under lib\project\character.ex

---
+++ project/lib/project/character.ex
@@ -0,0 +1,11 @@
+defmodule Project.Character do
+  defstruct title: nil,
+  first_name: nil,
+  middle_name: nil,
+  last_name: nil,
+  birth_date: nil,
+  age: nil,
+  gender: nil,
+  address: nil,
+  phone: nil
+end
